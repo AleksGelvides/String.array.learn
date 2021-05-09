39258356b94118dbc4781d70e9346bcc59ec7486
AGCPA <60176859+AGCPA@users.noreply.github.com>
2021-05-09 12:16:08 +0700

    На проверку.

--- src/Main.java
+++ src/Main.java
@@ -13,24 +13,28 @@ public class Main {
             namePerson = scanner.nextLine();
             if (checkUserName() == true) {
                 System.out.println("okey");
+                System.out.println("Ваше имя имеет длину " + namePerson.length() + " Символов");
+                namePerson.trim();
+                System.out.println("А без пробелов ваше имя занимает " + namePerson.length() + " символов");
                 n++;
             } else if (checkUserName() == false) {
                 System.out.println("Rename please");}
+            System.out.println("Массив: " + Arrays.toString(names));
         }
     }
     public static boolean checkUserName(){
         boolean checkName = true;
         boolean report_checkUserName = true;
         while (checkName){
-            if (names[0] == null){
+            if (names[0] == null) {
                 System.out.println("Nice name: 1");
                 names[n] = namePerson;
                 break;
-            } else if (namePerson.equalsIgnoreCase(names[n-1])){ //Почему не работает ни преинкримент, не постинкримент
-                report_checkUserName = !report_checkUserName;
-                System.out.println("This name already used"); // При отработке этого сценария, показывается 2 раза подряд.
-                break;
-            } else if (!(namePerson.equalsIgnoreCase(names[n-1]))){
+            } else if (namePerson.equalsIgnoreCase(names[n-1])) { //Почему не работает ни преинкримент, не постинкримент
+            report_checkUserName = !report_checkUserName;
+            System.out.println("This name already used"); // При отработке этого сценария, показывается 2 раза подряд.
+            break;
+            } else if (!(namePerson.equalsIgnoreCase(names[n-1]))){ //Почему не работает ни преинкримент, не постинкримент
                 System.out.println("Nice name");
                 names[n] = namePerson;
                 break;
